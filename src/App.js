737191fd3131cc17c2022b1bcfe7108ac56351c7
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-16 15:32:22 +0100

    mvp mostly done

--- src/App.js
+++ src/App.js
@@ -5,26 +5,42 @@ import {useState} from 'react'
 
 
 function App() {
-const [todoList,setTodoList] = useState([])
+const [todos,setTodos] = useState([])
 const [newItem, setNewItem] = useState("")
 
-const handleForm = (event)=>{
-
+const handleInput = (event)=>{
   setNewItem(event.target.value)
 }
 
 const handleSave = (event)=>{
   event.preventDefault()
-
+  const newTodo = {id: Date.now(), name: newItem}
+  const newTodoList = [...todos, newTodo]
+  setTodos(newTodoList)
+  setNewItem("")
 }
+
+const handleCompletion = (key)=>{
+  const newList = todos.filter((todo)=> todo.id !== key)
+  setTodos(newList)
+}
+
+const todoList = todos.map((todo)=>{
+  return (
+    <li key={todo.id}>
+      {todo.name}
+      <button onClick={()=>handleCompletion(todo.id)}>Complete</button>
+    </li>
+  )
+})
 
   return (
 <>    
 <h1>ToDo's</h1>
 <form onSubmit={handleSave}>
   <label htmlFor="addTodo">Todos to be added here: </label>
-  <input id="addTodo" type="text" value={newItem}/>
-  <input type="submit" value="Save Item" onInput={handleForm}/>
+  <input id="addTodo" type="text" value={newItem} onInput={handleInput}/>
+  <input type="submit" value="Save Item"/>
 </form>
 
 <ul>
